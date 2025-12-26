a564732b3dc862c801f0550094c1b7f65976a0f9
KirilPecev <kirilpecev@gmail.com>
2025-12-26 19:23:04 +0200

    null warnings

--- ECommercePlatform/CatalogService/Contracts/Requests/AddProductVariantRequest.cs
+++ ECommercePlatform/CatalogService/Contracts/Requests/AddProductVariantRequest.cs
@@ -2,9 +2,9 @@
 {
     public record AddProductVariantRequest()
     {
-        public string Sku { get; internal set; }
+        public string Sku { get; internal set; } = default!;
         public decimal Amount { get; internal set; }
-        public string Currency { get; internal set; }
+        public string Currency { get; internal set; } = default!;
         public int StockQuantity { get; internal set; }
         public string? Size { get; internal set; }
         public string? Color { get; internal set; }

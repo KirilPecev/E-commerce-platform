5b00a90cbfc04d7ac7d19ceb50cea221f75d56f8
KirilPecev <kirilpecev@gmail.com>
2026-01-12 23:02:52 +0200

    added payment completed event consumer

---
+++ ECommercePlatform/OrderService/Infrastructure/Messaging/Consumers/PaymentCompletedEventConsumer.cs
@@ -0,0 +1,21 @@
+﻿using ECommercePlatform.Events.PaymentIntegrationEvents;
+
+using MassTransit;
+
+using MediatR;
+
+using OrderService.Application.Orders.Commands;
+
+namespace OrderService.Infrastructure.Messaging.Consumers
+{
+    public class PaymentCompletedEventConsumer
+        (IMediator mediator) : IConsumer<PaymentCompletedIntegrationEvent>
+    {
+        public async Task Consume(ConsumeContext<PaymentCompletedIntegrationEvent> context)
+        {
+            PaymentCompletedIntegrationEvent message = context.Message;
+
+            await mediator.Send(new PayOrderCommand(message.OrderId));
+        }
+    }
+}

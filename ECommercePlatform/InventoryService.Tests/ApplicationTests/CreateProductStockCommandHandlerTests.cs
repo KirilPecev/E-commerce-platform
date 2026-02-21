ca2ccae68147c33f07b44686822bf578fea9db03
KirilPecev <kirilpecev@gmail.com>
2026-02-21 13:28:02 +0200

    added inventory service tests and implemented the unit tests

---
+++ ECommercePlatform/InventoryService.Tests/ApplicationTests/CreateProductStockCommandHandlerTests.cs
@@ -0,0 +1,16 @@
+﻿using ECommercePlatform.Application.Interfaces;
+
+using Moq;
+
+namespace CatalogService.Tests.ApplicationTests
+{
+    public class CreateProductCommandHandlerTests
+    {
+        private readonly Mock<IEventPublisher> eventPublisherMock;
+
+        public CreateProductCommandHandlerTests()
+        {
+            eventPublisherMock = new Mock<IEventPublisher>();
+        }
+    }
+}

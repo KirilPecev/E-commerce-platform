d41b9545801172fedf0d4483b5d17dd8962e093e
KirilPecev <kirilpecev@gmail.com>
2026-02-25 21:56:48 +0200

    tests renaiming

--- ECommercePlatform.Tests/IdentityService.Tests/IntegrationTests/IdentityTests.cs
+++ ECommercePlatform.Tests/IdentityService.Tests/IntegrationTests/IdentityTests.cs
@@ -6,12 +6,35 @@ using FluentAssertions;
 using IdentityService.Application.Identity.Commands;
 
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity.Data;
 
-namespace IdentityService.Tests.ApplicationTests
+namespace IdentityService.Tests.IntegrationTests
 {
-    public class LoginTests : IClassFixture<IdentityWebApplicationFactory>
+    public class IdentityTests : IClassFixture<IdentityWebApplicationFactory>
     {
+        [Fact]
+        public async Task Register_Should_Create_User()
+        {
+            var factory = new IdentityWebApplicationFactory()
+                 .WithWebHostBuilder(b =>
+                 {
+                     b.UseEnvironment("Testing");
+                 });
+
+            var client = factory.CreateClient();
+
+            var request = new RegisterRequest()
+            {
+                Email = "testuser@test.com",
+                Password = "StrongPass123!"
+            };
+
+            var response = await client.PostAsJsonAsync(
+                "/api/identity/register", request, TestContext.Current.CancellationToken);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
         [Fact]
         public async Task Register_Then_Login_ShouldReturnToken()
         {
@@ -70,8 +93,6 @@ namespace IdentityService.Tests.ApplicationTests
 
             var client = factory.CreateClient();
 
-            using var scope = factory.Services.CreateScope();
-
             // Act
             Func<Task> act = async () => await client.PostAsJsonAsync("/api/identity/login", new
             {
@@ -79,7 +100,7 @@ namespace IdentityService.Tests.ApplicationTests
                 Password = "Password123!"
             },
             TestContext.Current.CancellationToken);
-            
+
             // Assert
             await act.Should().ThrowAsync<UnauthorizedAccessException>();
         }

75eab9d67774649f6857d5539a9e60ee7c2ad2c5
KirilPecev <kirilpecev@gmail.com>
2026-03-25 23:05:37 +0200

    added outbox message model

---
+++ ECommercePlatform/ECommercePlatform/Data/Configuration/OutboxMessageConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using ECommercePlatform.Data.Models;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarRentalSystem.Data.Configuration
+{
+    public class OutboxMessageConfiguration : IEntityTypeConfiguration<OutboxMessage>
+    {
+        public void Configure(EntityTypeBuilder<OutboxMessage> builder)
+        {
+            builder
+                .HasKey(m => m.Id);
+
+            builder
+                .Property<string>("serializedData")
+                .IsRequired()
+                .HasField("serializedData");
+
+            builder
+                .Property(m => m.Type)
+                .IsRequired()
+                .HasConversion(
+                    t => t.AssemblyQualifiedName,
+                    t => Type.GetType(t));
+        }
+    }
+}
---
+++ ECommercePlatform/ECommercePlatform/Data/MessageDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CarRentalSystem.Data.Configuration;
+
+using ECommercePlatform.Data.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommercePlatform.Data
+{
+    public class MessageDbContext : DbContext
+    {
+        protected MessageDbContext(DbContextOptions<MessageDbContext> options)
+            : base(options) { }
+
+        public DbSet<OutboxMessage> OutboxMessages { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new OutboxMessageConfiguration());
+
+            builder.ApplyConfigurationsFromAssembly(this.ConfigurationsAssembly);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}

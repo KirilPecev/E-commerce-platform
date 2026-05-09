f3fc6e691d1a45a6ca2724d88af1aae04fc511c3
KirilPecev <kirilpecev@gmail.com>
2026-05-09 12:52:45 +0300

    added apy gateway using ocelot

---
+++ ECommercePlatform/ApiGateway/Program.cs
@@ -0,0 +1,24 @@
+using ECommercePlatform.Identity;
+
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder
+    .Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddOcelot();
+
+builder
+    .Services
+    .AddOcelot(builder.Configuration);
+
+builder
+    .Services
+    .AddTokenAuthentication(builder.Configuration);
+
+var app = builder.Build();
+
+await app.UseOcelot();
+await app.RunAsync();

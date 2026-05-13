d3983b8ea65ba9bb71711ef8c12e8b99b934a77c
KirilPecev <kirilpecev@gmail.com>
2026-05-13 22:14:49 +0300

    added payment refund and failed event consumers

---
+++ ECommercePlatform/OrderService/Infrastructure/Messaging/Consumers/PaymentFailedEventConsumer.cs
@@ -0,0 +1,21 @@
+﻿using ECommercePlatform.Events.PaymentIntegrationEvents;
+
+using MassTransit;
+
+using MediatR;
+
+using OrderService.Application.Orders.Commands;
+
+namespace OrderService.Infrastructure.Messaging.Consumers
+{
+    public class PaymentFailedEventConsumer
+        (IMediator mediator) : IConsumer<PaymentFailedIntegrationEvent>
+    {
+        public async Task Consume(ConsumeContext<PaymentFailedIntegrationEvent> context)
+        {
+            PaymentFailedIntegrationEvent message = context.Message;
+
+            await mediator.Send(new CancelOrderCommand(message.OrderId, message.FailureReason ?? "Failed to process payment"));
+        }
+    }
+}
---
+++ ECommercePlatform/OrderService/Infrastructure/Messaging/Consumers/PaymentRefundedEventConsumer.cs
@@ -0,0 +1,21 @@
+﻿using ECommercePlatform.Events.PaymentIntegrationEvents;
+
+using MassTransit;
+
+using MediatR;
+
+using OrderService.Application.Orders.Commands;
+
+namespace OrderService.Infrastructure.Messaging.Consumers
+{
+    public class PaymentRefundedEventConsumer
+        (IMediator mediator) : IConsumer<PaymentRefundedIntegrationEvent>
+    {
+        public async Task Consume(ConsumeContext<PaymentRefundedIntegrationEvent> context)
+        {
+            PaymentRefundedIntegrationEvent message = context.Message;
+
+            await mediator.Send(new CancelOrderCommand(message.OrderId, "Order refunded"));
+        }
+    }
+}
